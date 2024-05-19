57e5aefa7c090f5dd232802fc30a9ca48f3bb305
Burak Kabakcı <emrekabakci@gmail.com>
2024-05-19 23:17:42 +0100

    fix compile error

--- components/richPresenceList.tsx
+++ components/richPresenceList.tsx
@@ -1,8 +1,6 @@
 import type {
   LetterboxdRecentMovies,
   NotionDoingNow,
-  SpotifyCurrentTrack,
-  SpotifyRecentTracks,
   SteamRecentlyPlayedGames,
 } from '../types/rich-presence'
 import { Letterboxd16, Notion16, Spotify16, Steam16 } from './icons'
@@ -10,8 +8,6 @@ import { PresenceWidget } from './presenceWidget'
 
 interface RichPresenceListProps {
   presenceList: Array<
-    | SpotifyCurrentTrack
-    | SpotifyRecentTracks
     | LetterboxdRecentMovies
     | SteamRecentlyPlayedGames
     | NotionDoingNow
@@ -32,31 +28,7 @@ export function RichPresenceList({
       )}
       <div className="grid grid-cols-1 gap-4 mt-3 -mx-1 md:-mx-5 md:grid-cols-2 xl:grid-cols-2">
         {presenceList.map(rp => {
-          if (rp?.type === 'spotify_current') {
-            return (
-              <PresenceWidget
-                key={rp.type}
-                icon={<Spotify16 />}
-                verb={rp.verb}
-                title={rp.track.title}
-                subtitle={rp.track.artists}
-                img_url={rp.track.image}
-                url={rp.track.url}
-              />
-            )
-          } else if (rp?.type === 'spotify_recent') {
-            return (
-              <PresenceWidget
-                key={rp.type}
-                icon={<Spotify16 />}
-                verb={rp.verb}
-                title={rp.tracks[0]?.title}
-                subtitle={rp.tracks[0]?.artists}
-                img_url={rp.tracks[0]?.image}
-                url={rp.tracks[0]?.url}
-              />
-            )
-          } else if (rp?.type === 'letterboxd') {
+          if (rp?.type === 'letterboxd') {
             return (
               <PresenceWidget
                 key={rp.type}
